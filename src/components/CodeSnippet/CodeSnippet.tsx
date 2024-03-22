621169f581db658e879d5c4ffae8a8a0d0ace1fe
LeonAndrade <35314330+LeonAndrade@users.noreply.github.com>
2024-03-22 01:43:57 -0300

    Playing with custom code snippets

--- src/components/CodeSnippet/CodeSnippet.tsx
+++ src/components/CodeSnippet/CodeSnippet.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Code } from "bright";
+
 import styles from "./CodeSnippet.module.css";
+import { myTheme } from "./customTheme";
 
 function CodeSnippet(props) {
   return (
     <Code
-      {...props}
       className={styles.wrapper}
+      lineNumbers
+      theme={myTheme}
+      {...props}
     />
   );
 }
